8066a09071ccba0fefe9dfe30abad78972ef83a6
Paulo José dos Santos <paulo-santos_07@hotmail.com>
2024-11-01 15:47:34 -0300

    abrir e fechar modal

--- script.js
+++ script.js
@@ -7,5 +7,15 @@ let checkoutBtn = document.getElementById('checkout-btn')
 let closeModalBtn = document.getElementById('close-modal-btn')
 let cartCounter = document.getElementById('cart-count')
 let addressInput = document.getElementById('address')
-let addressWarn = docum.getElementById('address-warn')
+let addressWarn = document.getElementById('address-warn')
+
+cartBtn.addEventListener("click", function(){
+    cartModal.style.display = "flex"
+})
+
+cartModal.addEventListener("click", function(event){
+    if (event.target === cartModal || event.target === closeModalBtn){
+        cartModal.style.display = 'none'
+    }
+})
 
