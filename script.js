e95ef81b77de11fbec155eaa4ff6afc77484a090
Paulo José dos Santos <paulo-santos_07@hotmail.com>
2024-11-01 16:13:41 -0300

    add items cart

--- script.js
+++ script.js
@@ -9,8 +9,11 @@ let cartCounter = document.getElementById('cart-count')
 let addressInput = document.getElementById('address')
 let addressWarn = document.getElementById('address-warn')
 
+let cart = []
+
 cartBtn.addEventListener("click", function(){
     cartModal.style.display = "flex"
+    updateCartModal()
 })
 
 cartModal.addEventListener("click", function(event){
@@ -21,7 +24,6 @@ cartModal.addEventListener("click", function(event){
 
 menu.addEventListener("click", function(event){
     let parentButton = event.target.closest(".add-to-cart-btn")
-    console.log(parentButton);
     
     if (parentButton){
         const name = parentButton.getAttribute("data-name")
@@ -33,5 +35,45 @@ menu.addEventListener("click", function(event){
 })
 
 function addToCart(name, price){
-    alert("o item é " + name)
+    let existingItem = cart.find(item => item.name === name)
+
+    if(existingItem){
+        existingItem.quantity += 1
+        return
+    }else{
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        })
+    }
+
+    updateCartModal()
+}
+
+//atualiza o modal
+function updateCartModal(){
+    cartitemsContainer.innerHTML = ''
+    let total = 0
+
+    cart.forEach(item => {
+        let cartitemsElement = document.createElement("div")
+
+        cartitemsElement.innerHTML = `
+            <div>
+                <div>
+                    <p>${item.name}</p>
+                    <p>${item.quantity}</p>
+                    <p>R$ ${item.price}</p>
+                </div>
+
+                <div>
+                    <button>
+                        Remover
+                    </button>
+                </div>
+            </div>
+        `
+        cartitemsContainer.appendChild(cartitemsElement)
+    })
 }
